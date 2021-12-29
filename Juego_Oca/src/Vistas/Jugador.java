f3534776019dfdc9aa1131077eebb95fe1724af2
alaybandai44 <alvaro2107@gmail.com>
2021-12-29 16:42:52 +0100

    Clase Jugador

--- Juego_Oca/src/Vistas/Jugador.java
+++ Juego_Oca/src/Vistas/Jugador.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package Vistas;
-
+import Controladores.ControladorJugador;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 /**
  *
  * @author Javier_p
  */
-public class Jugador {
+public class Jugador extends JLabel{
+     private String nombre;
+    public ControladorJugador con;
     
+    public Jugador(String nombre, ImageIcon imagenFicha){
+        this.nombre = nombre;
+        con = new ControladorJugador(this);
+        this.setIcon(imagenFicha);
+    }
+
+    /**
+     * Metodo que retorna el nombre del jugador
+     * @return 
+     */
+    public String getNombre() {
+        return nombre;
+    }
 }
